4d394e0acb51456196df8906a934bdc45dabd9a5
jbbarth <jeanbaptiste.barth@gmail.com>
2013-04-29 00:00:00 +0200

    Refactor access to brute_force_counter

--- lib/redmine_stronger/account_controller_patch.rb
+++ lib/redmine_stronger/account_controller_patch.rb
@@ -4,9 +4,7 @@ class AccountController
   def invalid_credentials_with_locking
     if user = User.active.find_by_login(params[:username].to_s)
       #increment brute-force counter
-      pref = user.pref
-      pref[:brute_force_counter] = pref[:brute_force_counter].to_i + 1
-      pref.save
+      set_brute_force_counter(user, get_brute_force_counter(user) + 1)
       #lock the user immediately if detecting a brute force attack
       user.lock! if brute_forcing?(user)
     end
@@ -16,9 +14,7 @@ class AccountController
   alias_method_chain :invalid_credentials, :locking
 
   def successful_authentication_with_locking(user)
-    pref = user.pref
-    pref[:brute_force_counter] = 0
-    pref.save
+    set_brute_force_counter(user, 0)
     successful_authentication_without_locking(user)
   end
   alias_method_chain :successful_authentication, :locking
@@ -27,5 +23,15 @@ class AccountController
   private
   def brute_forcing?(user)
     user.pref[:brute_force_counter].to_i >= 3
+  end
+
+  def set_brute_force_counter(user, value)
+    pref = user.pref
+    pref[:brute_force_counter] = value
+    pref.save
+  end
+
+  def get_brute_force_counter(user)
+    user.pref[:brute_force_counter].to_i
   end
 end
