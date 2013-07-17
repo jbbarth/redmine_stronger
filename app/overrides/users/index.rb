b8a67e8a0bdb5f7fb0f59f1ffb48b443c8da4802
jbbarth <jeanbaptiste.barth@gmail.com>
2013-07-17 08:35:59 +0200

    Add lock reason beside username in users/index view

---
+++ app/overrides/users/index.rb
@@ -0,0 +1,10 @@
+Deface::Override.new :virtual_path  => 'users/index',
+                     :name          => 'add-lock-info-beside-locked-users',
+                     :insert_top    => 'td.username',
+                     :text          => %(<% if user.locked? && user.try(:lock_comment).present? %>
+                                           <div class=contextual>
+                                             <%= content_tag(:span, "",
+                                                             :class => "icon icon-help",
+                                                             :title => user.lock_comment) %>
+                                           </div>
+                                         <% end %>)
