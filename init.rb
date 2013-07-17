97d0217d335c80e43b5ce7813eb3fe72b8323eed
jbbarth <jeanbaptiste.barth@gmail.com>
2013-07-17 08:19:21 +0200

    Add deface gem so we can override part of core views easily

--- init.rb
+++ init.rb
@@ -1,3 +1,10 @@
+# Little hack for deface in redmine:
+# - redmine plugins are not railties nor engines, so deface overrides are not detected automatically
+# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
+# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
+Rails.application.paths["app/overrides"] ||= []
+Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
+
 Redmine::Plugin.register :redmine_stronger do
   name "Redmine Stronger plugin"
   description "This plugin adds some security features to Redmine"
