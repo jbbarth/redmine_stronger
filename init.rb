f3d6e6e3e228d9cfbef872442294fde4ed2ea083
Vincent Robert <vincent.robert@nanego.com>
2014-09-09 16:00:20 +0200

    Avoid requiring the same plugin twice

--- init.rb
+++ init.rb
@@ -4,7 +4,6 @@ Redmine::Plugin.register :redmine_stronger do
   author "Jean-Baptiste BARTH"
   author_url "mailto:jeanbaptiste.barth@gmail.com"
   requires_redmine :version_or_higher => "2.1.0"
-  requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
   version "0.0.1"
   url "https://github.com/jbbarth/redmine_stronger"
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
