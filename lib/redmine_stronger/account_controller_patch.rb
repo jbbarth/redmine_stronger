ca2f5e47326f2b6f1a9aa2740755f712ff811ad3
Mauryl Dovand <m.dovand@adacis.net>
2021-07-12 10:41:20 +0200

    users are now locked for certain amount of time (20 minutes)

--- lib/redmine_stronger/account_controller_patch.rb
+++ lib/redmine_stronger/account_controller_patch.rb
@@ -5,6 +5,7 @@ module PluginStronger
 
     # Maximum number of failed attempts before locking
     MAX_FAILED_ATTEMPTS = 5
+    LOCKED_FOR_MINUTES = 20
 
     # Patch #invalid_credentials to add a brute force attack counter
     #
@@ -16,14 +17,28 @@ module PluginStronger
         set_brute_force_counter(user, get_brute_force_counter(user) + 1)
         #lock the user immediately if detecting a brute force attack
         if brute_forcing?(user)
+          set_brute_force_lock_time(user, Time.now + LOCKED_FOR_MINUTES.minutes)
           user.update_attribute(:lock_comment, "Locked at #{Time.now} after #{MAX_FAILED_ATTEMPTS} erroneous password")
           user.lock!
         end
       end
-      #original action
+      # original action
       super
+      flash.now[:error] = l(:notice_account_invalid_credentials_or_locked)
     end
 
+    def account_locked(user, redirect_path=signin_path)
+      if get_brute_force_lock_time(user)&.past?
+        # reactivate user when lock time is past
+        user.update_attribute(:lock_comment, nil)
+        user.activate!
+        set_brute_force_lock_time(user, nil)
+        successful_authentication(user)
+      else
+        flash[:error] = l(:notice_account_invalid_credentials_or_locked)
+        redirect_to redirect_path
+      end
+    end
     # Patch #successful_authentication to reset brute force attack counter
     #
     # On successful authentication, brute_force_counter should be reset to 0 so
@@ -35,6 +50,7 @@ module PluginStronger
     end
 
     private
+
     def brute_forcing?(user)
       user.pref[:brute_force_counter].to_i >= MAX_FAILED_ATTEMPTS
     end
@@ -47,6 +63,16 @@ module PluginStronger
 
     def get_brute_force_counter(user)
       user.pref[:brute_force_counter].to_i
+    end
+
+    def set_brute_force_lock_time(user, time)
+      pref = user.pref
+      pref[:brute_force_lock_time] = time
+      pref.save
+    end
+
+    def get_brute_force_lock_time(user)
+      user.pref[:brute_force_lock_time]
     end
   end
 end
