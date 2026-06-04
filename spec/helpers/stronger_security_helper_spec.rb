a3bc7f014a1788c9a5cef7b9f5d79163c3f2f131
Vincent <contact@vincent-robert.com>
2026-06-04 13:30:00 +0200

    Fix specs when the sudo plugin is not present

--- spec/helpers/stronger_security_helper_spec.rb
+++ spec/helpers/stronger_security_helper_spec.rb
@@ -3,29 +3,24 @@
 require "spec_helper"
 
 describe StrongerSecurityHelper, type: :helper do
-  fixtures :users
-
   describe "#stronger_admin_badge" do
     it "returns a badge for an admin user" do
-      badge = helper.stronger_admin_badge(User.find_by_login("admin"))
+      badge = helper.stronger_admin_badge(User.new(admin: true))
       expect(badge).to include("stronger-admin-badge")
       expect(badge).to include(I18n.t(:stronger_admin_badge))
     end
 
     it "returns a badge for a sudoer who is not currently admin" do
-      user = User.find_by_login("jsmith")
-      user.update_column(:admin, false)
-      user.update_column(:sudoer, true) if user.respond_to?(:sudoer)
+      skip "redmine_sudo not installed" unless User.new.respond_to?(:sudoer=)
+
+      user = User.new(admin: false)
+      user.sudoer = true
 
       expect(helper.stronger_admin_badge(user)).to include("stronger-admin-badge")
     end
 
-    it "returns nil for a regular user" do
-      user = User.find_by_login("jsmith")
-      user.update_column(:admin, false)
-      user.update_column(:sudoer, false) if user.respond_to?(:sudoer)
-
-      expect(helper.stronger_admin_badge(user)).to be_nil
+    it "returns nil for a regular non-admin user" do
+      expect(helper.stronger_admin_badge(User.new(admin: false))).to be_nil
     end
 
     it "returns nil for a nil user" do
