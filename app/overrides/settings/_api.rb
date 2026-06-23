5576b6243232d7ef196b8fb7da4a7d6a9a74694b
Vincent <contact@vincent-robert.com>
2026-06-23 10:53:53 +0200

    Fix API settings tab hint

--- app/overrides/settings/_api.rb
+++ app/overrides/settings/_api.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(
   virtual_path:  'settings/_api',
   name:          'stronger-api-tab-hint',
-  insert_after:  'div.box.tabular.settings',
+  insert_after:  "erb[loud]:contains('submit_tag')",
   text: <<~ERB
-    <p class="icon icon-help">
+    <p class="stronger-api-hint">
       <%= l(:stronger_api_tab_hint_html,
             link: link_to(l(:stronger_api_tab_hint_link),
                           plugin_settings_path(id: 'redmine_stronger'))).html_safe %>
