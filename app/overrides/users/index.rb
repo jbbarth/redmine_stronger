948034dd3882ed6223a0cbafbbfc444e349405b8
Vincent <contact@vincent-robert.com>
2026-07-22 08:45:11 +0200

    Migrate legacy icon classes to SVG sprite_icon for Redmine 7.0

--- app/overrides/users/index.rb
+++ app/overrides/users/index.rb
@@ -3,8 +3,8 @@ Deface::Override.new :virtual_path  => 'users/index',
                      :insert_top    => 'td.username',
                      :text          => %(<% if user.locked? && user.try(:lock_comment).present? %>
                                            <div style="float:right;">
-                                             <%= content_tag(:span, "",
-                                                             :class => "icon icon-help",
+                                             <%= content_tag(:span, sprite_icon('help'),
+                                                             :class => "icon",
                                                              :title => user.lock_comment) %>
                                            </div>
                                          <% end %>)
